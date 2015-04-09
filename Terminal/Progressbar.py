5bcbb41413b34fecdc9a0a5e4080fe88184bcc3c
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2015-04-10 00:45:11 +0430

    progressbar in terminal

---
+++ Terminal/Progressbar.py
@@ -0,0 +1,20 @@
+import time
+import sys
+
+sys.stdout.write("[==          ] 20%")
+time.sleep(1)
+sys.stdout.write("\r")
+
+sys.stdout.write("[====        ] 40%")
+time.sleep(1)
+sys.stdout.write("\r")
+
+sys.stdout.write("[======      ] 60%")
+time.sleep(1)
+sys.stdout.write("\r")
+
+sys.stdout.write("[=========   ] 80%")
+time.sleep(1)
+sys.stdout.write("\r")
+
+sys.stdout.write("[============] 100%")
